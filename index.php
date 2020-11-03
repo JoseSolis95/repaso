10cd0f55ad5039ad63aec306f148f48ec7c9cca1
Jose Solis <jesh95@hotmail.com>
2020-11-03 15:31:31 -0500

    Mi primer commit del proyecto repaso

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Repaso Seminario</title>
+    <!--archivos de estilos-->
+</head>
+<body>
+    <h1>HOLA MUNDO</h1>
+
+    <!--Archivos de JavaScript-->
+</body>
+</html>
